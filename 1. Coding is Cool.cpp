f12fbe3e2d49502ca1b585adaf25dd6af9563a4f
Mitu291993 <mituullah@gmail.com>
2025-11-21 01:20:07 +0000

    Add files via upload

---
+++ 1. Coding is Cool.cpp
@@ -0,0 +1,10 @@
+//The blanks in the code below is cout << "coding is cool" 
+#include <iostream>  // This lets us use cout to print messages on the screen
+using namespace std; // This saves us from writing "std::" before cout and endl
+
+int main() {
+    // This line prints the message "coding is cool" followed by a new line
+    cout << "coding is cool" << endl;
+
+    // The program ends here
+}
